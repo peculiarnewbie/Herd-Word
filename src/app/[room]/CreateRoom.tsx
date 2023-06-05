c65f3f3a4ffed2ec42f1185698314f88134f7b65
peculiarnewbie <curiousarmand@gmail.com>
2023-06-05 23:14:37 +0700

    add slider and dropdown identifier

--- src/app/[room]/CreateRoom.tsx
+++ src/app/[room]/CreateRoom.tsx
@@ -9,61 +9,56 @@ import RoomParams from './RoomParams';
 
 export default function CreateRoom({roomId, playerId} : {roomId:any, playerId:string}){
 
-  const [hotJoinable, setHotJoinable] = useState(true);
+  
+  const [isHerd, setIsHerd] = useState(true);
 
     //@ts-ignore
     const CallCreateRoom = async (event) =>{
         event.preventDefault();
 
-        const input = event.target.answer.value
+        console.log(event.target.herdEndCondition.getAttribute('aria-valuenow'))
+        console.log(event.target.herdScoreTarget)
 
-        console.log("sending input");
+        const hotJoinable = event.target.hotJoinable['data-state']
+
+        let params = {}
+
+        if(isHerd)
+
+        console.log("creating room");
     
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "text/plain");
+        // var myHeaders = new Headers();
+        // myHeaders.append("Content-Type", "text/plain");
         
-        var raw = JSON.stringify({
-            "roomId": `${roomId}`,
-            "userId": `${playerId}`,
-            "input": `${input}`
-          });
+        // var raw = JSON.stringify({
+        //     "roomId": `${roomId}`,
+        //     "userId": `${playerId}`,
+        //     "input": `${input}`
+        //   });
         
-        var requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: raw,
-          next: { revalidate: 0 }
-        };
+        // var requestOptions = {
+        //   method: 'POST',
+        //   headers: myHeaders,
+        //   body: raw,
+        //   next: { revalidate: 0 }
+        // };
         
-        //@ts-ignore
-        const result = await fetch("https://ng51i1t4j1.execute-api.ap-southeast-1.amazonaws.com/Prod/roomactions/sendinput", requestOptions)
-          .then(response => response.text())
+        // //@ts-ignore
+        // const result = await fetch("https://ng51i1t4j1.execute-api.ap-southeast-1.amazonaws.com/Prod/roomactions/sendinput", requestOptions)
+        //   .then(response => response.text())
 
         
 
-        console.log(JSON.parse(result).body)
+        // console.log(JSON.parse(result).body)
     }
 
-    const HandleHotJoinCheckBox = (event:any) => {
-      console.log("changing state")
-      setHotJoinable(!hotJoinable)
-    }
+    
 
     if(roomId){
       return(
         <>
           <p>creating a room with id: {roomId}</p>
           <Form.Root className="FormRoot" onSubmit={CallCreateRoom}>
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <Checkbox.Root className="CheckboxRoot" defaultChecked id="c1" checked={hotJoinable} onCheckedChange={HandleHotJoinCheckBox}>
-                  <Checkbox.Indicator className="CheckboxIndicator">
-                    <CheckIcon />
-                  </Checkbox.Indicator>
-                </Checkbox.Root>
-                <label className="Label" htmlFor="c1">
-                  Allow hot-joining
-                </label>
-              </div>
               <RoomParams></RoomParams>
               <FormButton withButton={true} label='Create Room'></FormButton>
           </Form.Root>
@@ -76,16 +71,7 @@ export default function CreateRoom({roomId, playerId} : {roomId:any, playerId:st
         <p>aloo</p>
         <Form.Root className="FormRoot" onSubmit={CallCreateRoom}>
             <NameField withButton={false} label='Room Name' message='Please enter room name'></NameField>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <Checkbox.Root className="CheckboxRoot" defaultChecked id="c1" checked={hotJoinable} onCheckedChange={HandleHotJoinCheckBox}>
-                <Checkbox.Indicator className="CheckboxIndicator">
-                  <CheckIcon />
-                </Checkbox.Indicator>
-              </Checkbox.Root>
-              <label className="Label" htmlFor="c1">
-                Allow hot-joining
-              </label>
-            </div>
+            
             <FormButton withButton={true} label='Create Room'></FormButton>
         </Form.Root>
       </>
--- src/app/[room]/RoomParams.tsx
+++ src/app/[room]/RoomParams.tsx
@@ -2,6 +2,7 @@ import { useState, forwardRef } from "react";
 import * as Select from '@radix-ui/react-select';
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
+import * as Slider from '@radix-ui/react-slider';
 import './styles.css';
 
 const gameTypes = {'0':'Default Herd',
@@ -16,8 +17,11 @@ const herdEndConditions = {'0':'Default',
             
 
 export default function RoomParams(){
+    const [hotJoinable, setHotJoinable] = useState(true);
     const [gameType, setGameType] = useState('0');
     const [onlyHighest, setOnlyHighest] = useState(true);
+
+    
 
     const [herdEnds, setHerdEnds] = useState('0');
 
@@ -25,13 +29,42 @@ export default function RoomParams(){
         setOnlyHighest(!onlyHighest)
     }
 
+    const HandleHotJoinCheckBox = (event:any) => {
+        setHotJoinable(!hotJoinable)
+      }
+
     return(
         <>
-            <label className="Label" htmlFor="c1">
+            <HotJoinCheck></HotJoinCheck>
+            <GameTypeDropDown></GameTypeDropDown>
+            
+            <GameParams></GameParams>
+        </>
+    )
+
+    function HotJoinCheck(){
+        return(
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Checkbox.Root className="CheckboxRoot" defaultChecked id="hotJoinable" checked={hotJoinable} onCheckedChange={HandleHotJoinCheckBox}>
+                    <Checkbox.Indicator className="CheckboxIndicator">
+                    <CheckIcon />
+                    </Checkbox.Indicator>
+                </Checkbox.Root>
+                <label className="Label" htmlFor="c1">
+                    Allow hot-joining
+                </label>
+            </div>
+        )
+    }
+
+    function GameTypeDropDown(){
+        return(
+            <>
+                <label className="Label" htmlFor="c1">
                 Game Type:
             </label>
             <Select.Root value={gameTypes['0']} onValueChange={setGameType}>
-                <Select.Trigger className="SelectTrigger" aria-label="Type">
+                <Select.Trigger aria-valuenow={parseInt(gameType)} id="gameType" className="SelectTrigger" aria-label="Type">
                 <Select.Value placeholder="Select a game type">
                     {/* @ts-ignore */}
                     {gameTypes[gameType]}
@@ -90,30 +123,47 @@ export default function RoomParams(){
                 </Select.Content>
                 </Select.Portal>
             </Select.Root>
-            <GameParams></GameParams>
-        </>
-    )
+            </>
+        )
+    }
+
+    function ValueSlider({id, defaultValue, min, max}: {id:string, defaultValue:number, min:number, max:number}){
+        const [sliderValue, setSliderValue] = useState([defaultValue])
+        return(
+            <div style={{ display: 'flex', alignItems: 'center', gap:"1rem"}}>
+                <Slider.Root name={id} className="SliderRoot" defaultValue={sliderValue} min={min} max={max} step={1} onValueChange={setSliderValue}>
+                <Slider.Track className="SliderTrack">
+                    <Slider.Range className="SliderRange" />
+                </Slider.Track>
+                <Slider.Thumb  className="SliderThumb" aria-label="Volume" />
+                </Slider.Root>
+                <p>{sliderValue}</p>
+            </div>
+            )
+    }
     
     
     function GameParams(){
+        
         if(gameType == '0'){
             return(
                 <>
                     <p>Playing Herd Game with Default Parameters</p>
+                    
                 </>
             )
 
         }
         else if(gameType == '1'){
             return(
-                <div className="GameParams">
+                <div className="GameParams" id="GameParams">
                     <p>Choose Parameters</p>
                     <div style={{ display: 'flex', alignItems: 'center' }}>
                     <label className="Label" htmlFor="c1">
                         End of game condition:
                     </label>
                     <Select.Root value={herdEndConditions['0']} onValueChange={setHerdEnds}>
-                        <Select.Trigger className="SelectTrigger" aria-label="Type">
+                        <Select.Trigger aria-valuenow={parseInt(herdEnds)} id="herdEndCondition" className="SelectTrigger" aria-label="Type">
                         <Select.Value placeholder="Select a game type">
                             {/* @ts-ignore */}
                             {herdEndConditions[herdEnds]}
@@ -148,6 +198,17 @@ export default function RoomParams(){
                         </Select.Portal>
                     </Select.Root>
                     </div>
+                    {
+                        herdEnds == '1' && (
+                            <ValueSlider id="herdScoreTarget" defaultValue={5} min={3} max={10}></ValueSlider>
+                            
+                            )
+                        }
+                    {
+                        herdEnds == '2' && (
+                            <ValueSlider id="herdRoundTarget" defaultValue={15} min={5} max={30}></ValueSlider>
+                        )
+                    }
                     <div style={{ display: 'flex', alignItems: 'center' }}>
                     <Checkbox.Root className="CheckboxRoot" defaultChecked id="c1" checked={onlyHighest} onCheckedChange={HandleOnlyHighest}>
                     <Checkbox.Indicator className="CheckboxIndicator">
